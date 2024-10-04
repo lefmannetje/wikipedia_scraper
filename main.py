17c8aa5e5851c86459ab5af1aa3bf5a62c49d1a3
Levin <lefmannetje@gmail.com>
2024-10-04 13:35:41 +0200

    Revert "multithreading"
    This reverts commit c7406751653a23a45e184a75d222372283134e69.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import concurrent.futures
-import scraper
-import time
 import requests
+from pprint import pprint
+import time
+from bs4 import BeautifulSoup
+import scraper
+import sys
 import json
+
+start_time = time.time()
+
 
 # URL and endpoint definitions
 root_url = "https://country-leaders.onrender.com/"
@@ -16,48 +21,41 @@ leader_url = root_url + 'leader/'
 session = requests.Session()
 
 scraper.check_status(session, status_url)
+
 cookies = scraper.get_cookie(session, cookie_url)
+
 countries = scraper.get_countries(cookies, session, countries_url)
 
 leaders_per_country = {}
 
-# Define a function to process each country (to be used in parallel)
-def process_country(country):
-    country_leaders = []
-    
-    leaders = scraper.get_leaders(country, cookies, session, leaders_url)
-    
-    for leader in leaders:
-        leader_id = leader['id']
-        wiki_url = scraper.get_leader_wiki(leader_id, cookies, leader_url, session)
+for country in countries:        
+    if scraper.check(country, ['be', 'fr']):
+        continue
+    else:
+        leaders = scraper.get_leaders(country, cookies, session, leaders_url)
+        country_leaders = []
         
-        if wiki_url:
-            first_par = scraper.find_first_bold_paragraph(wiki_url, session)
-            leader_info = {
-                'id': leader_id,
-                'name': f"{leader['first_name']} {leader['last_name']}",
-                'wikipedia_url': wiki_url,
-                'first_paragraph': first_par['paragraph_text'] if isinstance(first_par, dict) else first_par
-            }
-            country_leaders.append(leader_info)
-    
-    return country, country_leaders
-
-# Use ThreadPoolExecutor to fetch leaders in parallel for each country
-start_time = time.time()
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    results = executor.map(process_country, countries)
-
-# Combine results into the leaders_per_country dictionary
-for country, leaders in results:
-    leaders_per_country[country] = leaders
+        for leader in leaders:
+            leader_id = leader['id']
+            wiki_url = scraper.get_leader_wiki(leader_id, cookies, leader_url, session)
+            
+            if wiki_url:
+                first_par = scraper.find_first_bold_paragraph(wiki_url, session)
+                leader_info = {
+                    'id': leader_id,
+                    'name': f"{leader['first_name']} {leader['last_name']}",
+                    'wikipedia_url': wiki_url,
+                    'first_paragraph': first_par['paragraph_text'] if isinstance(first_par, dict) else first_par
+                }
+                country_leaders.append(leader_info)
+        
+        leaders_per_country[country] = country_leaders
 
 scraper.create_leaders_json(leaders_per_country)
 
 end_time = time.time()
 print("It took ", end_time-start_time, "seconds to scrape Wiki!")
 
-# Read and display the data
 leaders_data = scraper.read_leaders_json()
 if leaders_data:
     print("Data read from leaders.json:")
